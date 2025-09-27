5605b21536611bce9b3174f6459e1ebf1152e638
soullmon <soulsollomon@gmail.com>
2025-09-27 14:49:31 +0700

    Update download.js

--- download.js
+++ download.js
@@ -3,8 +3,8 @@ document.getElementById("downloadBtn").addEventListener("click", function () {
   let ctx = canvas.getContext("2d");
 
   // size(1200x1200)
-  canvas.width = 1200;
-  canvas.height = 1200;
+  canvas.width = 1000;
+  canvas.height = 1000;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   let withSrc = categories.filter(id => document.getElementById(id).src);
