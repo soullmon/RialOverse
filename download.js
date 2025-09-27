08e6d6116c50bb1048d16013d1dbc35a1ab55384
soullmon <soulsollomon@gmail.com>
2025-09-27 17:37:33 +0700

    Update download.js

--- download.js
+++ download.js
@@ -1,36 +1,35 @@
-document.getElementById("downloadBtn").addEventListener("click", function () {
-  let canvas = document.getElementById("canvas"); 
+function downloadCharacter() {
+  let canvas = document.getElementById("canvas");
   let ctx = canvas.getContext("2d");
 
-  // ukuran 1000x1000
+  // Set ukuran canvas sesuai character
   canvas.width = 1000;
   canvas.height = 1000;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  let withSrc = categories.filter(id => document.getElementById(id).src);
+  // Ambil semua layer character
+  let layers = document.querySelectorAll("#character img");
   let loaded = 0;
 
-  if (withSrc.length === 0) return;
-
-  withSrc.forEach(id => {
-    let img = document.getElementById(id);
+  layers.forEach(img => {
     if (img.src) {
       let temp = new Image();
-      temp.crossOrigin = "anonymous"; // aman untuk CORS
+      temp.crossOrigin = "anonymous";
       temp.src = img.src;
+
       temp.onload = function () {
         ctx.drawImage(temp, 0, 0, canvas.width, canvas.height);
         loaded++;
 
-        if (loaded === withSrc.length) {
-          // konversi langsung ke dataURL
-          let dataURL = canvas.toDataURL("image/png");
+        if (loaded === layers.length) {
           let link = document.createElement("a");
           link.download = "character.png";
-          link.href = dataURL;
-
-          if (typeof link.download !== "undefined") {
-            // download langsung (desktop browser)
-            document.body.appendChild(link);
-            link.
-
+          link.href = canvas.toDataURL("image/png");
+          link.click();
+        }
+      };
+    } else {
+      loaded++;
+    }
+  });
+}
