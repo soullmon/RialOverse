6f6c8a446521cebbcc68ba8c13a860c4bbb94bd6
soullmon <soulsollomon@gmail.com>
2025-09-27 17:53:47 +0700

    Update download.js

--- download.js
+++ download.js
@@ -2,34 +2,50 @@ function downloadCharacter() {
   let canvas = document.getElementById("canvas");
   let ctx = canvas.getContext("2d");
 
-  // Set ukuran canvas sesuai character
+  // Atur ukuran hasil gambar (misalnya 1200x1200 px)
   canvas.width = 1000;
   canvas.height = 1000;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Ambil semua layer character
+  // Ambil semua layer di #character
   let layers = document.querySelectorAll("#character img");
+
+  // Filter hanya yang punya src (aktif)
+  let activeLayers = Array.from(layers).filter(img => img.src);
+
+  console.log("Layer aktif:", activeLayers.length);
+
   let loaded = 0;
+  let images = [];
 
-  layers.forEach(img => {
-    if (img.src) {
-      let temp = new Image();
-      temp.crossOrigin = "anonymous";
-      temp.src = img.src;
+  activeLayers.forEach((img, index) => {
+    let temp = new Image();
+    temp.crossOrigin = "anonymous";
+    temp.src = img.src;
 
-      temp.onload = function () {
-        ctx.drawImage(temp, 0, 0, canvas.width, canvas.height);
-        loaded++;
-
-        if (loaded === layers.length) {
-          let link = document.createElement("a");
-          link.download = "character.png";
-          link.href = canvas.toDataURL("image/png");
-          link.click();
-        }
-      };
-    } else {
+    temp.onload = function () {
+      images[index] = temp; // simpan urutan gambar sesuai layer
       loaded++;
-    }
+
+      console.log(`Layer berhasil dimuat: ${img.id}`);
+
+      // Kalau semua layer selesai dimuat, gambar semuanya ke canvas
+      if (loaded === activeLayers.length) {
+        images.forEach(image => {
+          ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+        });
+
+        console.log("Semua layer sudah digambar. Membuat file PNG...");
+        let link = document.createElement("a");
+        link.download = "character.png";
+        link.href = canvas.toDataURL("image/png");
+        link.click();
+      }
+    };
+
+    temp.onerror = function () {
+      loaded++;
+      console.warn(`Layer gagal dimuat: ${img.id}`);
+    };
   });
 }
